a9854ab4de07f6a940c10838b217b90289b86311
Joel S <joel29dec@gmail.com>
2019-03-17 17:52:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// add solution here
+
+function theBeatlesPlay (arr1, arr2){
+  let arr = []
+  for (let i = 0; i < arr1.length; i++){
+    arr.push(arr1[i] + " plays " + arr2[i])
+  }
+  return arr
+}
+
+
+function johnLennonFacts (arr){
+  let arr1 = []
+  let i = 0
+  do {
+    arr1.push(arr[i] + "!!!")
+    i++
+  } while (i < arr.length)
+  return (arr1)
+} 
+
+function iLoveTheBeatles(num){
+  let arr1 = []
+  let i = 0
+  do {
+    arr1.push("I love the Beatles1")
+    i++
+  } while (i < 15)
+  return (arr1)
+} 
